8e2fcb4082e3c34624c82c03f6adfde609eb795f
MadsonMagno <madson.luiz@hotmail.com>
2020-01-22 09:50:01 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -8,10 +8,29 @@ $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);
 */
-
+//carrega um usuario
+/*
 $root = new Usuario();
 $root->loadById(4);
 
-echo $root;
+echo $root;*/
+
+//Carrega uma lista de usuarios
+/*$lista  = Usuario::getList();
+
+echo json_encode($lista);*/
+
+//Carrega uma lista de usuarios, buscando pelo login
+
+/*$search = Usuario::search("jo");
+
+echo json_encode($search);*/
+
+
+//carrega um usuario autenticado
+
+$usuario = new Usuario();
+$usuario->login("jose", "123456789");
+echo "$usuario";
 
  ?>
